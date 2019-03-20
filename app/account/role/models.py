2b3b70a87e8f710cdb272981dc6512eaf481ef1b
Tim <sarrtim@gmail.com>
2019-03-20 14:02:36 +0300

    Implement AccountRole model fields and & add the model methods

--- app/account/role/models.py
+++ app/account/role/models.py
@@ -1,5 +1,18 @@
 from app.core.models import Base
-
+from app import db
 
 class AccountRole(Base):
-    pass
+
+    __tablename__ = 'account_roles'
+
+    name = db.Column(db.String(255))
+
+    def __init__(self, name=None):
+        self.name = name
+
+    def __repr__(self):
+        return "%s(%s)" % (self.__class__.__name__, self.name)
+
+    @classmethod
+    def get_by_name(cls, name):
+        return cls.query.filter_by(name=name).first()
