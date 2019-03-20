2190a8a7b78d62234dcbd5f52188e3b21b1f39c1
Tim <sarrtim@gmail.com>
2019-03-20 14:05:34 +0300

    Add implementation for AccountUserRole model and add relationships

--- app/account/user/role/models.py
+++ app/account/user/role/models.py
@@ -1,5 +1,22 @@
 from app import db
+from app.account.role.models import AccountRole
+
 
 class AccountUserRole(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    pass
+
+    __tablename__ = 'account_user_roles'
+
+    user_id = db.Column(db.String(255), db.ForeignKey('account_users.id'), primary_key=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('account_roles.id'), primary_key=True)
+    created_date = db.Column(db.DateTime, default=db.func.current_timestamp())
+    updated_date = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+
+    user = db.relationship('AccountUser', backref='account_user', lazy=True)
+    role = db.relationship(AccountRole, backref='account_role', lazy=True)
+
+    def __init__(self, user_id=None, role_id=None):
+        self.user_id = user_id
+        self.role_id = role_id
+
+    def __repr__(self):
+        return "%s(%s)" % (self.__class__.__name__, self.user_id)
