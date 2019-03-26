04d4ce97dd4a7f5306eff667bd3830ba7688737a
Tim <sarrtim@gmail.com>
2019-03-26 21:46:52 +0300

    Add get_online_users class method

--- app/account/user/models.py
+++ app/account/user/models.py
@@ -1,3 +1,4 @@
+from app.account.user.authenticated.models import AccountUserAuthenticated
 from app.account.user.role.models import AccountUserRole
 from app.core.models import Base
 from app import db
@@ -20,6 +21,8 @@ class AccountUser(Base):
 
     roles = db.relationship(AccountUserRole, backref='account_user_roles', cascade="save-update, merge, delete",
                             lazy=True)
+    sessions = db.relationship(AccountUserAuthenticated, backref='account_user_sessions', cascade="save-update, merge, "
+                                                                                                  "delete", lazy=True)
 
     def __init__(self, first_name=None, middle_name=None, last_name=None, email=None, phone=None, password=None,
                  token=None, image=None, is_active=None):
@@ -35,6 +38,15 @@ class AccountUser(Base):
 
     def __repr__(self):
         return "%s(%s)" % (self.__class__.__name__, self.id)
+
+    @classmethod
+    def get_online_users(cls):
+        sessions = cls.sessions
+        if sessions:
+            results = [{"id": user.id, "name": "{} {}".format(user.first_name, user.last_name)} for user in sessions]
+            data = {"count": len(results), "results": results}
+            return data
+        return None
 
     @classmethod
     def get_user_by_email(cls, email):
