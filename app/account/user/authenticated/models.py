577534c3efafb6b104e5c06a694cbcdcf276861b
Tim <sarrtim@gmail.com>
2019-03-26 21:33:48 +0300

    Add AccountUserAuthenticated model to store user session

---
+++ app/account/user/authenticated/models.py
@@ -0,0 +1,22 @@
+from app import db
+
+
+class AccountUserAuthenticated(db.Model):
+
+    __tablename__ = 'account_user_roles'
+
+    user_id = db.Column(db.Integer, db.ForeignKey('account_users.id'), primary_key=True)
+    session_id = db.Column(db.String(255))
+    created_date = db.Column(db.DateTime, default=db.func.current_timestamp())
+    updated_date = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+
+    def __init__(self, user_id=None, session_id=None):
+        self.user_id = user_id
+        self.session_id = session_id
+
+    def __repr__(self):
+        return "%s(%s)" % (self.__class__.__name__, self.user_id)
+
+
+    def get_by_session_id(cls, sid):
+        return cls.query.filter_by(session_id=sid).first()
