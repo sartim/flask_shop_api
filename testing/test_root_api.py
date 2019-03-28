85a1ce78805ea4884680a1d08182e493ac1a5f52
Tim <sarrtim@gmail.com>
2019-03-28 20:34:16 +0300

    Update unittest to class and inherit from Base class

--- testing/test_root_api.py
+++ testing/test_root_api.py
@@ -1,8 +1,10 @@
 from app import app
+from testing.base import Base
 
 
-def test_root_api():
-    client = app.test_client()
-    r = client.get('/')
-    assert r.status_code, 200
-    assert r.data, b"Welcome!"
+class TestRootApi(Base):
+    def test_root_api(self):
+        client = app.test_client()
+        r = client.get('/')
+        assert r.status_code, 200
+        assert r.data, b"Welcome!"
