867d74f3a78823cc7047d3dce1201450f29dafb0
Tim <sarrtim@gmail.com>
2019-03-28 20:33:31 +0300

    Update name for sqlite file

--- app/config.py
+++ app/config.py
@@ -47,6 +47,6 @@ class TestingConfig(BaseConfig):
     DEBUG = False
     BCRYPT_LOG_ROUNDS = 1
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'dev.sqlite')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'test.sqlite')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
