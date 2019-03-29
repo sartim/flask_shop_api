7983a98bcde85dad6ee546cc03d26c3911378a85
Tim <sarrtim@gmail.com>
2019-03-29 20:38:19 +0300

    Add online_status_api import

--- app/api_imports.py
+++ app/api_imports.py
@@ -1,2 +1,2 @@
 from app.core import root_api
-from app.account.api import generate_jwt_api, refresh_jwt_api, user_api
+from app.account.api import generate_jwt_api, refresh_jwt_api, user_api, online_status_api
