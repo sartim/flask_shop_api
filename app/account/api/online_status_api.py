a613791ad9f4fa3e4bbed4290f959547e816336d
Tim <sarrtim@gmail.com>
2019-03-29 20:37:01 +0300

    Add online_status_api

---
+++ app/account/api/online_status_api.py
@@ -0,0 +1,16 @@
+from flask import jsonify
+from flask.views import MethodView
+from app import app
+from app.account.user.models import AccountUser
+
+
+class OnlineStatus(MethodView):
+    def get(self):
+        data = AccountUser.get_online_users()
+        print(data)
+        if data:
+            return jsonify(data)
+        return jsonify({"count": 0, "results": []})
+
+app.add_url_rule('/account/user/online/', view_func=OnlineStatus.as_view('online-status'),
+                 methods=['GET'])
