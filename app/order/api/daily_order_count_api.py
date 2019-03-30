1f93abdb02494e005eaadc75cef3bfaeb93ddcac
Tim <sarrtim@gmail.com>
2019-03-30 12:05:25 +0300

    Add daily_order_counts_api for databased graph

---
+++ app/order/api/daily_order_count_api.py
@@ -0,0 +1,15 @@
+from flask import jsonify
+from flask.views import MethodView
+from app import app
+from app.helpers import data_generator
+
+
+class DailyOrderCount(MethodView):
+    # @cross_origin()
+    # @jwt_required
+    def get(self):
+        return jsonify(data_generator.get_date_values())
+
+
+app.add_url_rule('/order/daily/count/',
+                 view_func=DailyOrderCount.as_view('daily-published-count'), methods=['GET'])
