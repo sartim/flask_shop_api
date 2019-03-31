897282c90772ecd82d1b723b791294c856066455
Tim <sarrtim@gmail.com>
2019-03-31 19:31:56 +0300

    Add status_id foreign key field

--- app/order/models.py
+++ app/order/models.py
@@ -7,11 +7,13 @@ class Order(Base):
 
     user_id = db.Column(db.Integer, db.ForeignKey('account_users.id'), primary_key=True)
     product_id = db.Column(db.Integer, db.ForeignKey('products.id'), primary_key=True)
+    status_id = db.Column(db.Integer, db.ForeignKey('order_statuses.id'), primary_key=True)
     quantity = db.Column(db.Integer)
 
-    def __init__(self, user_id=None, product_id=None, quantity=None):
+    def __init__(self, user_id=None, product_id=None, status_id=None, quantity=None):
         self.user_id = user_id
         self.product_id = product_id
+        self.status_id = status_id
         self.quantity = quantity
 
     def __repr__(self):
