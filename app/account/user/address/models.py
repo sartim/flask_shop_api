cb0c26886bad4044ac234701dedc2b7a991dc46e
Tim <sarrtim@gmail.com>
2019-03-31 18:56:03 +0300

    Update country_id field to country string field

--- app/account/user/address/models.py
+++ app/account/user/address/models.py
@@ -6,15 +6,15 @@ class AccountAddress(db.Model):
     __tablename__ = 'account_user_roles'
 
     user_id = db.Column(db.Integer, db.ForeignKey('account_users.id'), primary_key=True)
-    country_id = db.Column(db.Integer, db.ForeignKey('countries.id'), primary_key=True)
+    country = db.Column(db.String(255))
     city_or_state = db.Column(db.String(255))
     address = db.Column(db.String(255))
     created_date = db.Column(db.DateTime, default=db.func.current_timestamp())
     updated_date = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
 
-    def __init__(self, user_id=None, country_id=None, city_or_state=None, address=None):
+    def __init__(self, user_id=None, country=None, city_or_state=None, address=None):
         self.user_id = user_id
-        self.country_id = country_id
+        self.country = country
         self.city_or_state = city_or_state
         self.address = address
 
