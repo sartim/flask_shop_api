e73bc21f82b713a7b1e68438c69a2206e4ff1621
Tim <sarrtim@gmail.com>
2019-03-31 15:21:23 +0300

    Add ProductCategory model

--- app/product/category/models.py
+++ app/product/category/models.py
@@ -1,5 +1,14 @@
 from app.core.models import Base
+from app import db
 
 
-class Product(Base):
-    pass
+class ProductCategory(Base):
+    __tablename__ = 'product_categories'
+
+    name = db.Column(db.String(255))
+
+    def __init__(self, name=None):
+        self.name = name
+
+    def __repr__(self):
+        return "%s(%s)" % (self.__class__.__name__, self.id)
