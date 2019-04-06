42bb3f5f3fa1d3cd10ef74f8f6ef77699fb63ded
Tim <sarrtim@gmail.com>
2019-04-06 21:09:30 +0300

    Add ProductApi with GET implemented

---
+++ app/product/api/product_api.py
@@ -0,0 +1,33 @@
+from flask import jsonify, request
+from flask.views import MethodView
+from flask_cors import cross_origin
+from flask_jwt_extended import jwt_required
+from app import app
+from app.product.models import Product
+
+
+class ProductApi(MethodView):
+    @cross_origin()
+    @jwt_required
+    def get(self):
+        page = request.args.get('page')
+        products = Product.get_all(page)
+        return jsonify(products), 200
+
+    @cross_origin()
+    @jwt_required
+    def post(self):
+        return jsonify(), 201
+
+    @cross_origin()
+    @jwt_required
+    def put(self):
+        return jsonify(), 200
+
+    @cross_origin()
+    @jwt_required
+    def delete(self):
+        return jsonify(), 200
+
+
+app.add_url_rule('/product/', view_func=ProductApi.as_view('products'), methods=['GET', 'POST', 'PUT','DELETE'])
