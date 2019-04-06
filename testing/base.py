7b0ac8b7686547a6ef7f7d31f70849bc82c35021
Tim <sarrtim@gmail.com>
2019-04-06 21:19:02 +0300

    Fix failing unit tests due to missing model imports for migrations

--- testing/base.py
+++ testing/base.py
@@ -3,6 +3,10 @@ from app import app, db
 from app.account.role.models import AccountRole
 from app.account.user.models import AccountUser
 from app.account.user.role.models import AccountUserRole
+from app.product.models import Product
+from app.product.category.models import ProductCategory
+from app.order.models import Order
+from app.order.status.models import OrderStatus
 from app.helpers import utils
 from app.api_imports import *
 
