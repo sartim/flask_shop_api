3060d4433448902b387f289c9a93006856f90336
Tim <sarrtim@gmail.com>
2019-04-07 18:28:04 +0300

    Add api import for product_api

--- app/api_imports.py
+++ app/api_imports.py
@@ -1,3 +1,4 @@
 from app.core import root_api
 from app.account.api import generate_jwt_api, refresh_jwt_api, user_api, online_status_api
 from app.order.api import daily_order_count_api, order_api
+from app.product.api import product_api
