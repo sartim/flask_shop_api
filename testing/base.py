62e6d5b9b6a20fcd4d8234d12942d0cb78696b6b
Tim <sarrtim@gmail.com>
2019-04-10 13:57:41 +0300

    Update Base to include client, preload and add authorization header

--- testing/base.py
+++ testing/base.py
@@ -9,30 +9,23 @@ from app.order.models import Order
 from app.order.status.models import OrderStatus
 from app.helpers import utils
 from app.api_imports import *
+from manage import add_roles, add_demo_users
 
 
 class Base:
     @classmethod
     def setup_class(cls):
+        cls.client = app.test_client()
         with app.app_context():
             Migrate(app, db)
             upgrade()
             db.create_all()
-
-            objects = [
-                AccountRole(name='SUPERUSER'), AccountRole(name='ADMIN'), AccountRole(name='STAFF'),
-                AccountRole(name='CLIENT')
-            ]
-            db.session.bulk_save_objects(objects)
-            db.session.commit()
-
-            user = AccountUser(first_name="Test", last_name="User", email="test@mail.com",
-                               password=utils.generate_password_hash("letmein"), is_active=True)
-            db.session.add(user)
-            db.session.commit()
-            user_role = AccountUserRole(user_id=user.id, role_id=3)
-            db.session.add(user_role)
-            db.session.commit()
+            # Add pre-loaded data
+            add_roles()
+            add_demo_users()
+            # Generate token for authentication header
+            r = cls.client.post('/account/generate/jwt/', json=dict(email='demo@mail.com', password='qwertytrewq'))
+            cls.headers = {'Authorization': 'Bearer {}'.format(r.json['access_token'])}
 
     @classmethod
     def teardown_class(cls):
