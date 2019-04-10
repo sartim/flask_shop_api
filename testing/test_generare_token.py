489129bd3d25e99e2cce14ea7093e423f351bb56
Tim <sarrtim@gmail.com>
2019-04-10 14:00:01 +0300

    Update test_generate_token_api

--- testing/test_generare_token.py
+++ testing/test_generare_token.py
@@ -1,10 +1,8 @@
-from app import app
 from testing.base import Base
 
 
 class TestGenerateTokenApi(Base):
     def test_generate_token_api(self):
-        client = app.test_client()
-        r = client.post('/account/generate/jwt/', json=dict(email='test@mail.com', password='letmein'))
+        r = self.client.post('/account/generate/jwt/', json=dict(email='demo2@mail.com', password='qwertytrewq'))
         assert r.status_code == 200
         assert 'access_token' in r.json
