5899036567a5e96a9a6f2fc9f77561ffca31c9f0
Tim <sarrtim@gmail.com>
2019-04-10 13:58:58 +0300

    Use client app from self

--- testing/test_root_api.py
+++ testing/test_root_api.py
@@ -1,9 +1,8 @@
-from app import app
+from testing.base import Base
 
 
-class TestRootApi():
+class TestRootApi(Base):
     def test_root_api(self):
-        client = app.test_client()
-        r = client.get('/')
+        r = self.client.get('/')
         assert r.status_code == 200
         assert r.data == b"Welcome!"
