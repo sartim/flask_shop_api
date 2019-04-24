e4b04aaf9df96c8c5f10122cb16a4a516750c19d
Tim <sarrtim@gmail.com>
2019-04-24 15:35:45 +0300

    Add SearchableMixin helper

---
+++ app/core/mixins.py
@@ -0,0 +1,47 @@
+import os
+
+from app import db
+from app.core.indexing import add_to_index, remove_from_index, query_index
+
+
+class SearchableMixin:
+    @classmethod
+    def search(cls, expression, page, per_page):
+        ids, total = query_index(cls.__tablename__, expression, page, per_page, cls.__searchable__)
+        if total == 0:
+            return cls.query.filter_by(id=None), 0
+        when = []
+        for i in range(len(ids)):
+            when.append((ids[i], i))
+        return cls.query.filter(cls.id.in_(ids)).order_by(
+            db.case(when, value=cls.id)), total
+
+    @classmethod
+    def before_commit(cls, session):
+        session._changes = {
+            'add': list(session.new),
+            'update': list(session.dirty),
+            'delete': list(session.deleted)
+        }
+
+    @classmethod
+    def after_commit(cls, session):
+        for obj in session._changes['add']:
+            if isinstance(obj, SearchableMixin):
+                add_to_index(obj.__tablename__, obj)
+        for obj in session._changes['update']:
+            if isinstance(obj, SearchableMixin):
+                add_to_index(obj.__tablename__, obj)
+        for obj in session._changes['delete']:
+            if isinstance(obj, SearchableMixin):
+                remove_from_index(obj.__tablename__, obj)
+        session._changes = None
+
+    @classmethod
+    def reindex(cls):
+        for obj in cls.query:
+            add_to_index(cls.__tablename__, obj)
+
+if not os.environ.get('TEST') == "TRUE":
+    db.event.listen(db.session, 'before_commit', SearchableMixin.before_commit)
+    db.event.listen(db.session, 'after_commit', SearchableMixin.after_commit)
