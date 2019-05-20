da60b0260aaa527b6731fb3a9875aaba24019b01
Tim <sarrtim@gmail.com>
2019-05-20 21:12:26 +0300

    Add product_category_api module import

--- app/api_imports.py
+++ app/api_imports.py
@@ -1,4 +1,4 @@
 from app.core import root_api
 from app.account.api import generate_jwt_api, refresh_jwt_api, user_api, online_status_api
 from app.order.api import daily_order_count_api, order_api
-from app.product.api import product_api
+from app.product.api import product_api, product_category_api
