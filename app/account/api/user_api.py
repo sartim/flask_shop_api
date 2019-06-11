78acb00140824e2f0f1f496f8feb164bb94a7ea3
Tim <sarrtim@gmail.com>
2019-06-11 15:36:31 +0300

    Refactor user_api module

--- app/account/api/user_api.py
+++ app/account/api/user_api.py
@@ -6,6 +6,7 @@ from app import app
 from app.account.user.models import AccountUser
 from app.account.user.role.models import AccountUserRole
 from app.helpers import validator, utils
+from constants import Message
 
 
 class AccountView(MethodView):
@@ -23,21 +24,24 @@ class AccountView(MethodView):
             if user:
                 if user:
                     return jsonify(user)
-            app.logger.warning('{} submitted {} user id which does not exist'.format(logged_in_user.name, user_id))
+            app.logger.warning('{} submitted {} user id which does not exist'
+                               .format(logged_in_user.get_full_name, user_id))
             return jsonify(message='{} does not exist'.format(user_id))
         if email:
             account = AccountUser.get_user_by_email(email)
             user = AccountUser.get_user(account)
             if user:
                 return jsonify(user)
-            app.logger.warning('{} submitted {} email which does not exist'.format(logged_in_user.name, email))
+            app.logger.warning('{} submitted {} email which does not exist'
+                               .format(logged_in_user.get_full_name, email))
             return jsonify(message='{} does not exist'.format(email)), 404
         if phone:
             account = AccountUser.get_user_by_phone(phone)
             user = AccountUser.get_user(account)
             if user:
                 return jsonify(user)
-            app.logger.warning('{} submitted {} phone number which does not exist'.format(logged_in_user.name, phone))
+            app.logger.warning('{} submitted {} phone number which does not exist'
+                               .format(logged_in_user.get_full_name, phone))
             return jsonify(message='{} does not exist'.format(phone)), 404
         users = AccountUser.get_all(int(page) if page else None)
         return jsonify(users)
@@ -97,40 +101,38 @@ class AccountView(MethodView):
         if not body:
             validated = validator.field_validator(keys, {})
             if not validated["success"]:
-                app.logger.warning(
-                    '{logged_in_user} made request with invalid fields: \n {request_body}'
-                        .format(logged_in_user=logged_in_user.name, request_body=body))
+                app.logger.warning('{logged_in_user} made request with invalid fields: \n {request_body}'
+                                   .format(logged_in_user=logged_in_user.get_full_name, request_body=body))
                 return jsonify(validated['data']), 400
         if request.is_json:
             body = request.get_json()
             validated = validator.field_validator(keys, body)
             if not validated["success"]:
-                app.logger.warning('{} made request with invalid fields: \n {}'.format(logged_in_user.name, body))
+                app.logger.warning('{} made request with invalid fields: \n {}'
+                                   .format(logged_in_user.get_full_name, body))
                 return jsonify(validated['data']), 400
             account_user = AccountUser.get_by_id(body['id'])
             if account_user:
-                first_name = body['first_name']
-                middle_name = body['middle_name']
-                last_name = body['last_name']
-                email = body['email']
-                phone = body['phone']
-                old_password = body['old_password']
-                new_password = body['new_password']
-
-                if old_password and new_password:
-                    confirm_password = utils.check_password_hash(account_user.password, old_password)
-                    if not confirm_password:
-                        app.logger.warning(
-                            '{} submitted password which does not match existing password'.format(logged_in_user.name))
-                        return jsonify(message='Old password does not match existing password'), 400
-
-                password = utils.generate_password_hash(body['new_password'], app.config.get('BCRYPT_LOG_ROUNDS'))
-                user_by_email = AccountUser.get_user_with_unique(email=email)
+                first_name = body['first_name'] if 'first_name' in body else None
+                middle_name = body['middle_name'] if 'middle_name' in body else None
+                last_name = body['last_name'] if 'last_name' in body else None
+                email = body['email'] if 'email' in body else None
+                phone = body['phone'] if 'phone' in body else None
+                password = None
+                if 'old_password' and 'new_password' in body:
+                    if body['old_password'] and body['new_password']:
+                        confirm_password = utils.check_password_hash(account_user.password, body['old_password'])
+                        if not confirm_password:
+                            app.logger.warning('{} submitted password which does not match existing password'
+                                               .format(logged_in_user.get_full_name))
+                            return jsonify(message='Old password does not match existing password'), 400
+                    password = utils.generate_password_hash(body['new_password'], app.config.get('BCRYPT_LOG_ROUNDS'))
+                user_by_email = AccountUser.get_user_by_email(email=email)
                 if user_by_email:
                     message = 'User with the email {} exists'.format(email)
                     app.logger.warning(message)
                     return jsonify(message=message), 400
-                user_by_phone = AccountUser.get_user_with_unique(phone=phone)
+                user_by_phone = AccountUser.get_user_by_phone(phone=phone)
                 if user_by_phone:
                     message = 'User with the phone {} exists'.format(phone)
                     app.logger.warning(message)
@@ -144,19 +146,17 @@ class AccountView(MethodView):
                         account_user.phone = phone if phone else account_user.phone
                         account_user.password = password if password else account_user.password
                         account_user.save()
-                        user = {"id": account_user.id, "name": account_user.name, "email": account_user.email}
                         app.logger.debug("Successfully updated my user profile with the details: \n{}".format(body))
-                        return jsonify({"user": user}), 201
+                        return jsonify(message=Message.SUCCESS), 201
                 except Exception as e:
                     app.logger.exception('Exception occurred')
                     return jsonify(message='An error occurred. {}'.format(str(e))), 400
             app.logger.warning('{} trying to update user details with {} who does not exist'.
-                               format(logged_in_user.name, body['id']))
+                               format(logged_in_user.get_full_name, body['id']))
             return jsonify(message='User with id {} does not exist'.format(body['id'])), 404
         else:
             app.logger.warning('User submitted request with content type header not being application/json')
             return jsonify(message='Content-type header is not application/json'), 400
-
 
     @cross_origin()
     @jwt_required
