804d932ef202cdf622c604f8b48be43b9612ba9d
Tim <sarrtim@gmail.com>
2019-06-16 19:55:48 +0300

    Update test case for testing get all categories and add get by id test

--- tests/test_category_api.py
+++ tests/test_category_api.py
@@ -2,9 +2,18 @@ from tests.base import Base
 
 
 class TestProductCategoryApi(Base):
-    def test_get(self):
+    def test_get_all(self):
         req = self.client.get(self.product_category_url,
                               headers=self.headers)
         assert req.status_code == 200
         assert 'count' in req.json
         assert 'results' in req.json
+
+    def test_get_by_id(self):
+        req = self.client.get('{}?id=1'
+                              .format(self.product_category_url),
+                              headers=self.headers)
+        assert req.status_code == 200
+        assert 'name' in req.json
+        assert 'created_date' in req.json
+        assert 'updated_date' in req.json
