8d8f3d4993750f85bb0ecde52a8d13cc006b0eb3
Tim <sarrtim@gmail.com>
2019-06-18 20:09:08 +0300

    Add import for added model OrderItem

--- tests/base.py
+++ tests/base.py
@@ -8,6 +8,7 @@ from app.account.user.role.models import AccountUserRole
 from app.product.models import Product
 from app.product.category.models import ProductCategory
 from app.order.models import Order
+from app.order.item.models import OrderItem
 from app.order.status.models import OrderStatus
 from app.helpers import utils
 from app.api_imports import *
