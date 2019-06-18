02d3f406cbbe73f1b1e3a9dcc75291663f2977be
Tim <sarrtim@gmail.com>
2019-06-18 20:08:49 +0300

    Update field name on order model

--- app/order/models.py
+++ app/order/models.py
@@ -13,15 +13,19 @@ class Order(Base, SearchableMixin):
     __tablename__ = 'orders'
     __searchable__ = ['id']
 
-    user_id = db.Column(db.Integer, db.ForeignKey('account_users.id'), primary_key=True)
-    product_id = db.Column(db.Integer, db.ForeignKey('products.id'), primary_key=True)
-    status_id = db.Column(db.Integer, db.ForeignKey('order_statuses.id'), primary_key=True)
-    quantity = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('account_users.id'),
+                        primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'),
+                           primary_key=True)
+    status_id = db.Column(db.Integer, db.ForeignKey('order_statuses.id'),
+                          primary_key=True)
+    order_total = db.Column(db.DECIMAL(precision=10, scale=2))
 
     product = db.relationship(Product, backref='oder_product', lazy=True)
     status = db.relationship(OrderStatus, backref='oder_status', lazy=True)
 
-    def __init__(self, user_id=None, product_id=None, status_id=None, quantity=None):
+    def __init__(self, user_id=None, product_id=None, status_id=None,
+                 quantity=None):
         self.user_id = user_id
         self.product_id = product_id
         self.status_id = status_id
@@ -34,22 +38,26 @@ class Order(Base, SearchableMixin):
     def response(cls, orders):
         results = []
         for order in orders.items:
-            data = dict(id=order.id, product=order.product.name, status=order.status.name,  quantity=order.quantity,
-                    created_date=order.created_date)
+            data = dict(id=order.id, product=order.product.name,
+                        status=order.status.name,  quantity=order.quantity,
+                        created_date=order.created_date)
             results.append(data)
         data = cls.response_dict(orders, results, '/account/user/')
         return data
 
     @classmethod
     def get_all(cls, page):
-        orders = cls.query.paginate(page=page, per_page=int(os.environ.get('PAGINATE_BY')), error_out=True)
+        orders = cls.query.paginate(page=page,
+                                    per_page=int(os.environ.get('PAGINATE_BY')),
+                                    error_out =True)
         data = cls.response(orders)
         return data
 
     @classmethod
     def get_today(cls, page):
         orders = cls.query.filter(func.date(cls.created_date) == date.today())\
-            .paginate(page=page, per_page=int(os.environ.get('PAGINATE_BY')), error_out=True)
+            .paginate(page=page, per_page=int(os.environ.get('PAGINATE_BY')),
+                      error_out=True)
         data = cls.response(orders)
         return data
 
@@ -58,7 +66,8 @@ class Order(Base, SearchableMixin):
         yesterday = date.today() - timedelta(days=1)
         orders = cls.query.filter(func.date(cls.created_date) == yesterday) \
             .filter(cls.status_id == constants.COMPLETE) \
-            .paginate(page=page, per_page=int(os.environ.get('PAGINATE_BY')), error_out=True)
+            .paginate(page=page, per_page=int(os.environ.get('PAGINATE_BY')),
+                      error_out=True)
         data = cls.response(orders)
         return data
 
@@ -68,33 +77,41 @@ class Order(Base, SearchableMixin):
             .filter(extract('year', cls.created_date) == datetime.now().year) \
             .filter(extract('month', cls.created_date) == datetime.now().month) \
             .filter(cls.status_id == constants.COMPLETE) \
-            .paginate(page=page, per_page=int(os.environ.get('PAGINATE_BY')), error_out=True)
+            .paginate(page=page, per_page=int(os.environ.get('PAGINATE_BY')),
+                      error_out=True)
         data = cls.response(orders)
         return data
 
     @classmethod
     def get_last_month(cls, page):
         orders = cls.query\
-            .filter(extract('year', cls.created_date) == datetime.now().year) \
-            .filter(extract('month', cls.created_date) == datetime.now().month - 1) \
+            .filter(extract('year',
+                            cls.created_date) == datetime.now().year) \
+            .filter(extract('month',
+                            cls.created_date) == datetime.now().month - 1) \
             .filter(cls.status_id == constants.COMPLETE) \
-            .paginate(page=page, per_page=int(os.environ.get('PAGINATE_BY')), error_out=True)
+            .paginate(page=page, per_page=int(os.environ.get('PAGINATE_BY')),
+                      error_out=True)
         data = cls.response(orders)
         return data
 
     @classmethod
     def get_this_year(cls, page):
-        orders = cls.query.filter(extract('year', cls.created_date) == datetime.now().year) \
+        orders = cls.query.filter(extract('year',
+                                          cls.created_date) == datetime.now().year) \
             .filter(cls.status_id == constants.COMPLETE) \
-            .paginate(page=page, per_page=int(os.environ.get('PAGINATE_BY')), error_out=True)
+            .paginate(page=page, per_page=int(os.environ.get('PAGINATE_BY')),
+                      error_out=True)
         data = cls.response(orders)
         return data
 
     @classmethod
     def get_last_year(cls, page):
-        orders = cls.query.filter(extract('year', cls.created_date) == datetime.now().year - 1) \
+        orders = cls.query.filter(extract('year',
+                                          cls.created_date) == datetime.now().year - 1) \
             .filter(cls.status_id == constants.COMPLETE) \
-            .paginate(page=page, per_page=int(os.environ.get('PAGINATE_BY')), error_out=True)
+            .paginate(page=page, per_page=int(os.environ.get('PAGINATE_BY')),
+                      error_out=True)
         data = cls.response(orders)
         return data
 
