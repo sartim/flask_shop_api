10f13d5c4dc5faefbebb5068e4eed02f2098c373
Tim <sarrtim@gmail.com>
2019-09-21 16:47:38 +0300

    Refactor base

--- tests/base.py
+++ tests/base.py
@@ -7,13 +7,15 @@ from manage import add_roles, add_demo_users, add_product_data
 
 class Base:
     @classmethod
-    def setup_class(cls):
-        assert os.environ.get("TEST") == "TRUE"
-        cls.client = app.test_client()
+    def load_paths(cls):
         cls.root_url = '/'
         cls.generate_jwt_url = '/account/generate/jwt/'
         cls.user_url = '/account/user/'
         cls.product_category_url = '/product/category/'
+
+    @classmethod
+    def create_test_app(cls):
+        cls.client = app.test_client()
         with app.app_context():
             Migrate(app, db)
             upgrade()
@@ -23,8 +25,20 @@ class Base:
             add_demo_users()
             add_product_data()
             # Generate token for authentication header
-            req = cls.client.post('/account/generate/jwt/', json=dict(email='demo@mail.com', password='qwertytrewq'))
-            cls.headers = {'Authorization': 'Bearer {}'.format(req.json['access_token'])}
+            req = cls.client.post(
+                cls.generate_jwt_url,
+                json=dict(email='demo@mail.com', password='qwertytrewq')
+            )
+            cls.headers = {
+                'Authorization': 'Bearer {}'.format(
+                    req.json['access_token'])
+            }
+
+    @classmethod
+    def setup_class(cls):
+        assert os.environ.get("TEST") == "TRUE"
+        cls.load_paths()
+        cls.create_test_app()
 
     @classmethod
     def teardown_class(cls):
