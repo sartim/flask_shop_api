06d1a60d830cfb6270e81a0d45af6e3cf9000dda
Tim <sarrtim@gmail.com>
2019-09-21 16:58:50 +0300

    Refactor base model

--- app/core/models.py
+++ app/core/models.py
@@ -1,7 +1,11 @@
+from datetime import date, datetime
+
 import flask
 import os
 
-from sqlalchemy import desc
+from flask_jwt_extended import get_jwt_identity
+from sqlalchemy import desc, func, extract
+
 from app import db, app
 
 
@@ -9,32 +13,59 @@ class BaseModel(db.Model):
     __abstract__ = True
 
     id = db.Column(db.Integer, primary_key=True)
-    created_at = db.Column(db.DateTime(timezone=True), default=db.func.current_timestamp(), nullable=False)
-    updated_at = db.Column(db.DateTime(timezone=True), default=db.func.current_timestamp(),
-                           onupdate=db.func.current_timestamp(), nullable=False)
+    created_at = db.Column(
+        db.DateTime(timezone=True), default=db.func.current_timestamp()
+    )
+    updated_at = db.Column(
+        db.DateTime(timezone=True), default=db.func.current_timestamp(),
+        onupdate=db.func.current_timestamp()
+    )
 
     @classmethod
     def get_dict(cls, **kwargs):
         return dict(kwargs)
 
     @classmethod
-    def get_by_id(cls, id):
-        return cls.query.filter_by(id=id).first_or_404(description='No result found')
+    def response(cls, stitch):
+        raise NotImplemented
 
     @classmethod
-    def update(cls, id, **kwargs):
+    def get_by_id_data(cls, _id):
+        obj = cls.get_by_id(_id)
+        data = cls.response(obj)
+        return data
+
+    @classmethod
+    def get_by_id(cls, _id):
+        return cls.query.filter_by(id=_id) \
+            .first_or_404(description='No result found')
+
+    @classmethod
+    def get_current_user(cls):
+        return cls.query \
+            .filter_by(email=get_jwt_identity()).first()
+
+    @classmethod
+    def update(cls, _id, **kwargs):
         try:
-            cls.query.filter(cls.id == id).update(dict(**kwargs), synchronize_session='evaluate')
+            cls.query.filter(cls.id == _id) \
+                .update(dict(**kwargs), synchronize_session='evaluate')
             cls.save()
         except Exception as e:
-            app.logger.exception("Error occurred on update. {}".format(str(e)))
+            app.logger.exception("Error occurred on update. {}"
+                                 .format(str(e)))
             return False
         return True
 
     def create(self):
-        db.session.add(self)
-        save, message = self.save()
-        return save, message, self
+        try:
+            db.session.add(self)
+            self.save()
+        except Exception as e:
+            app.logger.exception("Error creating object - {}. {}"
+                                 .format(self.__name__, str(e)))
+            return False
+        return self
 
     @classmethod
     def get_or_create(cls, **kwargs):
@@ -63,67 +94,121 @@ class BaseModel(db.Model):
             db.session.delete(self)
             return True
         except Exception:
-            app.logger.exception('Could not delete {} instance.'.format(self.__name__))
+            app.logger.exception(
+                'Could not delete {} instance.'.format(self.__name__))
         return False
 
     @classmethod
     def save(cls):
         try:
             db.session.commit()
-            app.logger.debug('Successfully committed {} instance'.format(cls.__name__))
-        except Exception as e:
-            app.logger.exception('Exception occurred. Could not save {} instance.'.format(cls.__name__))
-            return False, "Error creating object - {}. {}".format(cls.__name__, str(e))
-        return True, 'Successfully committed {} instance'.format(cls.__name__)
+            app.logger.debug('Successfully committed {} instance'
+                             .format(cls.__name__))
+        except Exception:
+            app.logger.exception(
+                'Exception occurred. Could not save {} instance.'
+                    .format(cls.__name__)
+            )
 
     @classmethod
     def revert(cls):
         return db.session.rollback()
 
     @classmethod
-    def build_response(cls, obj, results, path, id=None, **kwargs):
+    def build_response(cls, obj, results, path, _id=None, **kwargs):
         domain = flask.request.url_root
         prev_url, next_url = None, None
-        if id:
-            next_url = "{}{}?id={}".format(domain, path, id)
+        if _id:
+            next_url = "{}{}?id={}".format(domain, path, _id)
         elif kwargs:
-            next_url = "{}{}?{}={}&page={}".format(domain, path,
-                                                   ' '.join(kwargs.keys()),
-                                                   kwargs[' '.join(kwargs.keys())],
-                                                   obj.next_num)
-        elif not id:
-            next_url = "{}{}?page={}".format(domain, path, obj.next_num)
+            next_url = "{}{}?{}={}&page={}".format(
+                domain, path, ' '.join(kwargs.keys()),
+                kwargs[' '.join(kwargs.keys())],
+                obj.next_num
+            )
+        elif not _id:
+            next_url = "{}{}?page={}".format(
+                domain, path, obj.next_num
+            )
 
         if kwargs:
-            prev_url = "{}{}?{}={}&page={}".format(domain, path,
-                                                   ' '.join(kwargs.keys()),
-                                                   kwargs[' '.join(kwargs.keys())],
-                                                   obj.prev_num)
-        elif not id:
-            prev_url = "{0}{1}?page={2}".format(domain, path, obj.prev_num)
-        elif id:
-            prev_url = "{}{}?id={}&page={}".format(domain, path, id, obj.prev_num)
+            prev_url = "{}{}?{}={}&page={}".format(
+                domain, path,
+                ' '.join(kwargs.keys()),
+                kwargs[' '.join(kwargs.keys())],
+                obj.prev_num
+            )
+        elif not _id:
+            prev_url = "{0}{1}?page={2}".format(
+                domain, path, obj.prev_num
+            )
+        elif _id:
+            prev_url = "{}{}?id={}&page={}".format(
+                domain, path, _id, obj.prev_num,
+                domain, path, _id, obj.prev_num
+            )
 
         if obj.has_next:
-            data = dict(count=obj.total, results=results, next=next_url,
-                        previous=prev_url if obj.prev_num else "")
+            data = dict(
+                count=obj.total, results=results, next=next_url,
+                previous=prev_url if obj.prev_num else ""
+            )
         else:
-            data = dict(count=obj.total, results=results, next="", previous="")
+            data = dict(
+                count=obj.total, results=results, next="", previous=""
+            )
         return data
 
     @classmethod
-    def build_paginated_response(cls, objects, url):
-        pass
-
-    @classmethod
     def get_all(cls, page):
-        objects = cls.query.order_by(desc(cls.created_at)). \
-            paginate(page=page,
-                     per_page=int(os.environ.get('PAGINATE_BY')),
-                     error_out=True)
-        return objects
+        query = cls.query.order_by(desc(cls.created_at))
+        results = cls.paginate_result(query, page)
+        return results
 
     @classmethod
     def get_all_data(cls, page):
         objects = cls.get_all(page)
-        return cls.build_paginated_response(objects, flask.request.full_path)
+        return cls.build_paginated_response(
+            objects, flask.request.full_path
+        )
+
+    @classmethod
+    def filter_all_today(cls):
+        return cls.query.filter(
+            func.date(cls.created_at) == date.today()
+        )
+
+    @classmethod
+    def filter_all_this_month(cls):
+        return cls.query.filter(
+            extract('month', cls.created_at) == datetime.now().month
+        )
+
+    @classmethod
+    def filter_all_last_month(cls):
+        return cls.query.filter(
+            extract('month', cls.created_at) == datetime.now().month - 1
+        )
+
+    @classmethod
+    def filter_all_this_year(cls):
+        return cls.query.filter(
+            extract('year', cls.created_at) == datetime.now().year
+        )
+
+    @classmethod
+    def paginate_result(cls, query, page):
+        return query.paginate(
+            page=page,
+            per_page=int(os.environ.get('PAGINATE_BY')),
+            error_out=True
+        )
+
+    @classmethod
+    def build_paginated_response(cls, objects, url):
+        results = []
+        for obj in objects.items:
+            data = cls.response(obj)
+            results.append(data)
+        data = cls.build_response(objects, results, url)
+        return data
