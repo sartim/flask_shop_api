45caed1ee1f39dd91488b90c80186f25faa0c7d3
Tim <sarrtim@gmail.com>
2019-12-11 15:07:48 +0300

    Update unittest for category api

--- tests/test_category_api.py
+++ tests/test_category_api.py
@@ -1,20 +1,21 @@
 from tests.base import Base
 
 
-class TestProductCategoryApi(Base):
+class TestCategoryApi(Base):
     def test_get_all(self):
-        req = self.client.get(self.product_category_url,
-                              headers=self.headers)
+        req = self.client.get(
+            self.product_category_url,
+            headers=self.headers
+        )
         assert req.status_code == 200
         assert 'count' in req.json
         assert 'results' in req.json
 
     def test_get_by_id(self):
-        req = self.client.get('{}?id=1'
-                              .format(self.product_category_url),
-                              headers=self.headers)
+        req = self.client.get(
+            '{}/{}'.format(self.product_category_url, 1),
+            headers=self.headers)
         assert req.status_code == 200
-        assert 'Electronics' == req.json['name']
         assert 'name' in req.json
         assert 'created_date' in req.json
         assert 'updated_date' in req.json
