fbe7c7a76d1cad166818f1c3561eeec5416b251b
Tim <sarrtim@gmail.com>
2019-12-11 15:15:15 +0300

    Update user api unit tests

--- tests/test_user_api.py
+++ tests/test_user_api.py
@@ -3,30 +3,54 @@ from app.core.constants import Message
 
 
 class TestUserApi(Base):
-    def test_get(self):
+    def test_get_all(self):
         req = self.client.get(self.user_url, headers=self.headers)
         assert req.status_code == 200
         assert 'count' in req.json
         assert 'results' in req.json
 
+    def test_get_by_id(self):
+        req = self.client.get(
+            "{}/{}".format(self.user_url, 1),
+            headers=self.headers)
+        assert req.status_code == 200
+        assert 'id' in req.json
+        assert 'first_name' in req.json
+        assert 'last_name' in req.json
+        assert 'email' in req.json
+        assert 'phone' in req.json
+        assert 'created_at' in req.json
+        assert 'updated_at' in req.json
+
+
     def test_post(self):
-        req = self.client.post(self.user_url,
-                               json=dict(first_name='Test',
-                                         last_name='User',
-                                         email='test1@mail.com',
-                                         phone='34534535',
-                                         password='test'))
+        req = self.client.post(
+            self.user_url,
+            json=dict(
+                first_name='Test',
+                last_name='User',
+                email='test1@mail.com',
+                phone='34534535',
+                password='test'
+            )
+        )
         assert req.status_code == 201
         assert req.json['message'] == 'Successfully saved new user'
 
     def test_put(self):
-        req = self.client.put(self.user_url, headers=self.headers,
-                              json=dict(id=1, last_name='User2'))
+        req = self.client.put(
+            "{}/{}".format(self.user_url, 1),
+            headers=self.headers,
+            json=dict(id=1, last_name='User2')
+        )
         assert req.status_code == 201
         assert req.json['message'] == Message.SUCCESS
 
     def test_delete(self):
-        req = self.client.delete(self.user_url, headers=self.headers,
-                                 json=dict(id=1))
+        req = self.client.delete(
+            "{}/{}".format(self.user_url, 1),
+            headers=self.headers,
+            json=dict(id=1)
+        )
         assert req.status_code == 200
         assert req.json['message'] == Message.SUCCESS
