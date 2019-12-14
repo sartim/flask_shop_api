b4bdf2098a68f1c5a7f7c7b413c9d934cdf59a5a
Tim <sarrtim@gmail.com>
2019-12-14 22:06:42 +0300

    dd helper for reading json file

--- app/core/helpers/utils.py
+++ app/core/helpers/utils.py
@@ -1,3 +1,4 @@
+import json
 import bcrypt
 import pycountry
 
@@ -32,3 +33,13 @@ def list_countries():
     :return countries:
     """
     return [{"country_id": country.numeric, 'country': country.name} for country in list(pycountry.countries)]
+
+
+def open_file(file_path, type_='json'):
+    """Reads file given"""
+    with open(file_path) as f:
+        if type_ == 'xml':
+            data = f.read()
+        else:
+            data = json.load(f)
+    return data
