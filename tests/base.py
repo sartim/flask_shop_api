d65e12d7a86e123414aa560641fb38d879692eaa
Tim <sarrtim@gmail.com>
2019-12-15 17:54:19 +0300

    Update setup_class

--- tests/base.py
+++ tests/base.py
@@ -1,5 +1,6 @@
 import os
 
+from unittest import mock
 from flask_migrate import Migrate, upgrade
 from app import app, db
 from app.user import urls
@@ -12,30 +13,37 @@ from manage import add_roles, add_demo_users, add_product_data
 class Base:
     @classmethod
     def setup_class(cls):
-        assert os.environ.get("TEST") == "TRUE"
+        if not os.environ.get('ENV'):
+            with mock.patch.dict(os.environ, {
+                'ENV': 'TEST'
+            }, clear=True):
+                # Assert if environment is set to TEST
+                assert os.environ.get("ENV") == "TEST"
+        else:
+            assert os.environ.get("ENV") == "TEST"
         cls.client = app.test_client()
         cls.root_url = '/'
-        cls.generate_jwt_url = '/account/generate/jwt/'
-        cls.user_url = '/account/user/'
-        cls.product_category_url = '/product/category/'
+        cls.generate_jwt_url = '/auth/generate-jwt'
+        cls.user_url = '/users'
+        cls.permission_url = '/permissions'
+        cls.role_url = '/roles'
+        cls.category_url = '/categories'
+        cls.status_url = '/statuses'
         with app.app_context():
             Migrate(app, db)
             upgrade()
             db.create_all()
-            # Add pre-loaded data
             add_roles()
             add_demo_users()
             add_product_data()
-            cls.root_url = '/'
-            cls.generate_jwt_url = '/auth/generate-jwt'
-            cls.user_url = '/users'
-            cls.permission_url = '/permissions'
-            cls.role_url = '/roles'
-            cls.category_url = '/categories'
-            cls.status_url = '/statuses'
             # Generate token for authentication header
-            req = cls.client.post('/account/generate/jwt/', json=dict(email='demo@mail.com', password='qwertytrewq'))
-            cls.headers = {'Authorization': 'Bearer {}'.format(req.json['access_token'])}
+            req = cls.client.post(
+                "/account/generate/jwt/", 
+                json=dict(email="demo@mail.com", password="qwertytrewq")
+            )
+            cls.headers = {
+                "Authorization": "Bearer {}".format(req.json["access_token"])
+            }
 
     @classmethod
     def teardown_class(cls):
