9e5118e760192a962df99bafcfa894b02049bf95
Tim <sarrtim@gmail.com>
2019-12-16 15:14:31 +0300

    Add status api unittest

---
+++ tests/test_status_api.py
@@ -0,0 +1,51 @@
+from app.core.helpers import utils
+from manage import add_order_statuses
+from tests.base import Base
+
+
+class TestStatusApi(Base):
+    def test_get_all(self):
+        self.headers["Content-type"] = "application/json"
+        req = self.client.get(self.status_url, headers=self.headers)
+        assert req.status_code == 200
+        assert "results" in req.json
+    
+    def test_get_by_id(self):
+        add_order_statuses()
+        self.headers["Content-type"] = "application/json"
+        req = self.client.get("{}/{}".format(
+            self.status_url, 1), headers=self.headers
+        )
+        assert req.status_code == 200
+        assert "name" in req.json
+        assert "description" in req.json
+
+    def test_post(self):
+        data = utils.open_file("data/statuses.json")
+        self.headers["Content-type"] = "application/json"
+        for item in data:
+            req = self.client.post(
+                self.status_url,
+                headers=self.headers,
+                json=item
+            )
+            assert req.status_code == 201
+            assert "message" in req.json
+            assert "Successfully Saved!" == req.json["message"]
+
+    def test_put(self):
+        self.headers["Content-type"] = "application/json"
+        payload = dict(description="Bank loan")
+        req = self.client.put(
+            "{}/{}".format(self.status_url, 1), headers=self.headers,
+            json=payload
+        )
+        assert req.status_code == 200
+        assert req.json["message"] == "Successfully Updated!"
+
+    def test_delete(self):
+        self.headers["Content-type"] = "application/json"
+        req = self.client.delete(
+            "{}/{}".format(self.status_url, 1), headers=self.headers
+        )
+        assert req.status_code == 204
