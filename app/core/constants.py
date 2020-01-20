3b4bde6038fa25be2dd1ac6acb00296ab457481d
Tim <sarrtim@gmail.com>
2020-01-20 18:38:13 +0300

    Add constants for response messages

--- app/core/constants.py
+++ app/core/constants.py
@@ -2,6 +2,15 @@ DRAFT = 1
 PENDING = 2
 COMPLETE = 3
 
+RECORD_SAVED = "Successfully saved record."
+RECORD_NOT_SAVED = "Record was not saved."
+RECORD_UPDATED = "Successfully updated record."
+RECORD_NOT_UPDATED = "Record was not updated."
+RECORD_DELETED = "Successfully deleted record."
+RECORD_NOT_DELETED = "Record was not deleted."
+RECORD_ALREADY_DELETED = "The record was deleted and is no longer available."
+
+
 class Message:
     ERROR = 'Error occurred'
     VALIDATION_ERROR = 'Field validation error'
