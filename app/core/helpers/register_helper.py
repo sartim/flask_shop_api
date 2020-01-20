caa778c8697516fc69fe872717bc62aa433c65d8
Tim <sarrtim@gmail.com>
2020-01-20 18:11:15 +0300

    Add register helper for registering endpoints

---
+++ app/core/helpers/register_helper.py
@@ -0,0 +1,43 @@
+from app import app
+
+
+def register_api(view, endpoint, url, pk='id', pk_type='int'):
+    view_func = view.as_view(endpoint)
+    app.add_url_rule(
+        url, defaults={pk: None},
+        view_func=view_func, methods=['GET', ]
+    )
+    app.add_url_rule(url, view_func=view_func, methods=['POST', ])
+    app.add_url_rule(
+        '{}/<{}:{}>'.format(url, pk_type, pk),
+        view_func=view_func,
+        methods=['GET', 'PUT', 'DELETE']
+    )
+
+
+def register_basic_api(view, endpoint, url, methods=None):
+    if methods is None:
+        methods = ['GET', ]
+    view_func = view.as_view(endpoint)
+    app.add_url_rule(
+        '{}'.format(url), view_func=view_func, methods=methods
+    )
+
+
+def register_complex_api(
+        view, endpoint, url_path_one, url_path_two,
+        pk='id', pk_type='int'):
+    view_func = view.as_view(endpoint)
+    app.add_url_rule(
+        '{}/<{}:{}>{}'.format(url_path_one, pk_type, pk, url_path_two),
+        view_func=view_func, methods=['GET', ]
+    )
+    app.add_url_rule(
+        '{}/<{}:{}>{}'.format(url_path_one, pk_type, pk, url_path_two),
+        view_func=view_func, methods=['POST', ]
+    )
+    app.add_url_rule(
+        '{}/<{}:{}>{}'.format(url_path_one, pk_type, pk, url_path_two),
+        view_func=view_func,
+        methods=['GET', 'PUT', 'DELETE']
+    )
