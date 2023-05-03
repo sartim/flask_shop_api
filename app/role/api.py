10c1464db89564a723bb313bdb915b1e5da6c005
Tim <sarrtim@gmail.com>
2023-05-03 17:18:15 +0300

    Update role api

--- app/role/api.py
+++ app/role/api.py
@@ -1,12 +1,20 @@
 from flask_cors import cross_origin
 from flask_jwt_extended import jwt_required
 
-from app.core.base_resource import BaseResource
-from app.role.models import Role
-from app.role.schemas import RoleSchema
+from app.core.base_resource import BaseResource, ChildBaseResource
+from app.role.models import Role, RolePermission
+from app.role.schemas import RoleSchema, role_args_schema, RolePermissionSchema
 
 
 class RoleApi(BaseResource):
-    decorators = [cross_origin(), jwt_required()]
-    schema = RoleSchema()
+    schema = RoleSchema
     model = Role
+    request_args = role_args_schema
+
+
+class RolePermissionApi(ChildBaseResource):
+    decorators = [cross_origin(), jwt_required()]
+    parent = Role
+    field = "role_id"
+    model = RolePermission
+    schema = RolePermissionSchema
