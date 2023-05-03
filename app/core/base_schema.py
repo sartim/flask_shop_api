11d82384ccec1092db197f88d0017b956118c3b4
Tim <sarrtim@gmail.com>
2023-05-03 17:11:04 +0300

    Update base schema

--- app/core/base_schema.py
+++ app/core/base_schema.py
@@ -1,6 +1,19 @@
 from marshmallow import fields
+from webargs import fields as fd
 
 
-class BaseSchema:
+class AbstractBaseSchema:
     created_at = fields.DateTime('%Y-%m-%d %H:%M:%S', dump_only=True)
     updated_at = fields.DateTime('%Y-%m-%d %H:%M:%S', dump_only=True)
+
+
+class BaseSchema(AbstractBaseSchema):
+    id = fields = fields.Int()
+
+
+base_args_schema = {
+    "page": fd.Int(),
+    "limit": fd.Int(),
+    "created_at": fd.DateTime(),
+    "updated_at": fd.DateTime()
+}
