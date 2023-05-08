bd76c1f3fcea25f6116fe61ac801c0b03fcf9b2e
Tim <sarrtim@gmail.com>
2023-05-08 11:14:02 +0300

    Update dotenv loader

--- app/core/config.py
+++ app/core/config.py
@@ -2,8 +2,7 @@ import os
 
 from dotenv import load_dotenv
 
-dotenv_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..', '.env'))
-load_dotenv(dotenv_path)
+load_dotenv()
 
 
 class BaseConfig(object):
