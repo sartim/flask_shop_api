bfa5268980760e3c19ccb163c75c169edee0d2f0
Tim <sarrtim@gmail.com>
2023-05-08 12:13:35 +0300

    Update path for category api

--- app/category/routes.py
+++ app/category/routes.py
@@ -1,4 +1,4 @@
 from app.category.api import CategoryApi
 from app.core.helpers.register_helper import register_api
 
-register_api(CategoryApi, 'category_api', '/categories', pk='category_id')
+register_api(CategoryApi, 'category_api', '/api/v1/categories', pk='category_id')
