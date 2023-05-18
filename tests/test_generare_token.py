b180a0ca10a15351b6c624394cd17070793be670
Tim <sarrtim@gmail.com>
2023-05-18 20:55:01 +0300

    Update credentials for testing generate token

--- tests/test_generare_token.py
+++ tests/test_generare_token.py
@@ -4,7 +4,7 @@ from tests.base import Base
 class TestGenerateTokenApi(Base):
     def test_generate_token_api(self):
         req = self.client.post(self.generate_jwt_url,
-                               json=dict(email='demo2@mail.com',
-                                         password='qwertytrewq'))
+                               json=dict(email="admin@mail.com",
+                                         password="admin_pass"))
         assert req.status_code == 200
         assert 'access_token' in req.json
