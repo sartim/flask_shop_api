78bd2caac609e9ca983136032eb1ca3e238e2ecf
Tim <sarrtim@gmail.com>
2024-03-02 17:32:04 +0300

    Update route helper

--- app/core/helpers/register_helper.py
+++ app/core/helpers/register_helper.py
@@ -1,53 +1,42 @@
-def register_api(blue_print, view, endpoint, url, pk='_id', pk_type='string'):
+from app.core.constants import ApiVersion
+
+
+def register_api(
+        blue_print,
+        view, endpoint, url, pk='_id', pk_type='int', version=ApiVersion.V1):
     view_func = view.as_view(endpoint)
     blue_print.add_url_rule(
-        url, defaults={pk: None},
+        "/api/{}{}".format(version, url), defaults={pk: None},
         view_func=view_func, methods=['GET', ]
     )
-    blue_print.add_url_rule(url, view_func=view_func, methods=['POST', ])
     blue_print.add_url_rule(
-        '{}/<{}:{}>'.format(url, pk_type, pk),
+        "/api/{}{}".format(version, url), view_func=view_func,
+        methods=['POST', ])
+    blue_print.add_url_rule(
+        '/api/{}{}/<{}:{}>'.format(version, url, pk_type, pk),
         view_func=view_func,
         methods=['GET', 'PUT', 'DELETE']
     )
 
 
-def register_basic_api(blue_print, view, endpoint, url, methods=None):
+def register_basic_api(
+        blue_print,
+        view, endpoint, url, methods=None, version=ApiVersion.V1):
     if methods is None:
         methods = ['GET', ]
     view_func = view.as_view(endpoint)
     blue_print.add_url_rule(
-        '{}'.format(url), view_func=view_func, methods=methods
+        '/api/{}{}'.format(version, url), view_func=view_func, methods=methods
     )
 
 
 def register_complex_api(
-        blue_print, view, endpoint, url_path_one, url_path_two,
-        pk='_id', pk_type='int'):
+        blue_print: object,
+        view: object, endpoint: object, url_path_one: object, url_path_two: object,
+        pk: object = '_id', pk_type: object = 'int', version: object = ApiVersion.V1) -> object:
     view_func = view.as_view(endpoint)
     blue_print.add_url_rule(
-        '{}/<{}:{}>{}'.format(url_path_one, pk_type, pk, url_path_two),
-        view_func=view_func, methods=['GET', ]
-    )
-    blue_print.add_url_rule(
-        '{}/<{}:{}>{}'.format(url_path_one, pk_type, pk, url_path_two),
-        view_func=view_func, methods=['POST', ]
-    )
-    blue_print.add_url_rule(
-        '{}/<{}:{}>{}'.format(url_path_one, pk_type, pk, url_path_two),
-        view_func=view_func,
-        methods=['GET', 'PUT', 'DELETE']
-    )
-
-
-def register_multiple_id_api(
-        blue_print, view, endpoint, url_path_one,
-        url_path_two, id_one, id_one_type, id_two, id_two_type):
-    view_func = view.as_view(endpoint)
-    blue_print.add_url_rule(
-        '{}/<{}:{}>{}/<{}:{}>'.format(
-            url_path_one, id_one_type, id_one,
-            url_path_two, id_two_type, id_two),
-        view_func=view_func,
-        methods=['GET', 'PUT', 'DELETE']
+        '/api/{}{}/<{}:{}>{}'.format(
+            version, url_path_one, pk_type, pk, url_path_two),
+        view_func=view_func, methods=['GET', 'PUT', 'DELETE']
     )
