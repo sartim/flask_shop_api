3d330f661cfe143f1bb89c7622be85697969f8f8
Tim <sarrtim@gmail.com>
2024-03-02 17:31:53 +0300

    Update route structure

--- app/order/routes.py
+++ app/order/routes.py
@@ -3,29 +3,31 @@ from flask import Blueprint
 from app.order.api import (
     OrderApi, OrderTotalSumTodayApi, OrderCountApi, OrderItemApi
 )
-from app.core.helpers.register_helper import register_api, register_basic_api
+from app.core.helpers.register_helper import (
+    register_api, register_basic_api, register_complex_api)
 
 order_api = Blueprint('order_api', __name__)
 
 register_api(
     order_api, OrderApi,
     'order_api',
-    '/api/v1/orders',
+    '/orders',
     pk='_id'
 )
 register_basic_api(
     order_api, OrderTotalSumTodayApi,
     'order_total_sum_today_api',
-    '/api/v1/orders/total-sum'
+    '/orders/total-sum'
 )
-register_basic_api(
+register_complex_api(
     order_api, OrderCountApi,
     'order_count_api',
-    '/api/v1/orders/<string:period>/count'
+    '/orders',
+    '/count'
 )
-register_basic_api(
+register_complex_api(
     order_api, OrderItemApi,
-    'order_items_api',
-    '/api/v1/orders/items',
-    methods=['POST']
+    'order_item_api',
+    '/orders',
+    '/items'
 )
--- app/status/routes.py
+++ app/status/routes.py
@@ -4,4 +4,4 @@ from app.core.helpers.register_helper import register_api
 
 status_api = Blueprint('status_api', __name__)
 
-register_api(status_api, StatusApi, 'status_api', '/api/v1/statuses', pk='_id')
+register_api(status_api, StatusApi, 'status_api', '/statuses', pk='_id')
